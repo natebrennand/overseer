be2fdf1cdff9f353f8fa80ea540ca1e45184502b
Nate Brennand <natebrennand@gmail.com>
2014-04-03 22:25:56 -0400

    watch multiple files

--- watch.go
+++ watch.go
@@ -13,34 +13,87 @@ const (
 	Delay = time.Second
 )
 
-// run whatever command was passed in
-func runCommand() {
-	c := exec.Command(os.Args[2], os.Args[3:]...)
-	var stdout bytes.Buffer
-	c.Stdout = &stdout
+var watchFiles []string
+var commandArgs []string
 
-	err := c.Run()
-	if err != nil {
-		log.Fatal(err.Error())
+func parseComands() {
+	commandArgs = []string{}
+	watchFiles = []string{}
+
+	onCommands := false
+	for _, w := range(os.Args) {
+		if w == "-c" {
+			onCommands = true
+			continue
+		}
+
+		if onCommands {
+			commandArgs = append(commandArgs, w)
+		} else {
+			watchFiles = append(watchFiles, w)
+		}
 	}
-
-	fmt.Println(stdout.String())
 }
 
-func main() {
-	f := os.Args[1]
-	lastModified := time.Now()
+func initFilesModTimes() map[string]time.Time {
+	fileModTimes := make(map[string]time.Time)
 
-	for {
-		fmt.Println("echo")
-		time.Sleep(Delay)
-		fi, err := os.Stat(f)
+	for _, f := range(watchFiles) {
+		fInfo, err := os.Stat(f)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
+		fileModTimes[f] = fInfo.ModTime()
+	}
 
-		if lastModified != fi.ModTime() {
-			lastModified = fi.ModTime()
+	return fileModTimes
+}
+
+func filesModified (fileModTimes map[string]time.Time) bool {
+	returnVal := false
+	for f := range(fileModTimes) {
+		fInfo, err := os.Stat(f)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		if fileModTimes[f] != fInfo.ModTime() {
+			fileModTimes[f] = fInfo.ModTime()
+			returnVal = true
+		}
+	}
+	return returnVal
+}
+
+// run whatever command was passed in when started
+func runCommand() {
+	var output bytes.Buffer
+	var c *exec.Cmd
+	if len(commandArgs) > 1 {
+		c = exec.Command(commandArgs[0], commandArgs[1:]...)
+	} else {
+		c = exec.Command(commandArgs[0])
+	}
+	c.Stdout = &output
+	c.Stderr = &output
+
+	err := c.Run()
+	if err != nil {
+		fmt.Println(output.String())
+		log.Fatal(err.Error())
+	}
+
+	if output.String() != "" {
+		fmt.Println(output.String())
+	}
+}
+
+func main() {
+	parseComands()
+	fileModTimes := initFilesModTimes()
+
+	for {
+		time.Sleep(Delay)
+		if filesModified(fileModTimes) {
 			runCommand()
 		}
 	}
